e1beeb497e354163b0862c07b222befbb8c30b00
21701 <king2doi@gmail.com>
2021-12-30 10:08:32 +0900

    Update object_detection_video.py

--- object_detection_video.py
+++ object_detection_video.py
@@ -46,3 +46,79 @@ def load_model(model_dir) :
     return detection_model
 
 detection_model = load_model(PATH_TO_MODEL_DIR)
+
+
+def show_inference(detection_model, image_np) :
+    # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
+    input_tensor = tf.convert_to_tensor(image_np)
+    # The model expects a batch of images, so add an axis with `tf.newaxis`.
+    input_tensor = input_tensor[tf.newaxis, ...]
+
+    # input_tensor = np.expand_dims(image_np, 0)
+    detections = detection_model(input_tensor)
+#     print(detections)
+    # All outputs are batches tensors.
+    # Convert to numpy arrays, and take index [0] to remove the batch dimension.
+    # We're only interested in the first num_detections.
+    num_detections = int(detections.pop('num_detections'))
+    detections = {key: value[0, :num_detections].numpy()
+                   for key, value in detections.items()}
+    detections['num_detections'] = num_detections
+
+    # detection_classes should be ints.
+    detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+    
+    print(detections)
+    image_np_with_detections = image_np.copy()
+
+    viz_utils.visualize_boxes_and_labels_on_image_array(
+          image_np_with_detections,
+          detections['detection_boxes'],
+          detections['detection_classes'],
+          detections['detection_scores'],
+          category_index,
+          use_normalized_coordinates=True,
+          max_boxes_to_draw=200,
+          min_score_thresh=.30,
+          agnostic_mode=False)
+
+    cv2.imshow( 'result' , image_np_with_detections)
+
+
+
+
+
+
+# 비디오를 실행하는 코드
+cap = cv2.VideoCapture('data/video.mp4')
+
+if cap.isOpened() == False :
+    print('비디오 실행에러')
+
+else :
+    # 비디오 캡쳐해서 이미지를 1장씩 가져온다
+    # 이 1장의 이미지를 오브젝트 디텍션 한다
+    while cap.isOpened() :
+        ret, frame = cap.read()
+
+        if ret == True :
+            # frame 이 이미지에 대한 넘파이 어레이 이므로
+            # 이 frame 을 오브젝트 디텔션 한다.
+
+    
+            # 학습용으로, 돋영상으로 저장하는 코드를
+            # 수정하세요 
+
+            # save_inference(detection_model, frame)
+
+            show_inference(detection_model, frame)
+
+            if cv2.waitKey(27) & 0xFF == 27 :
+                break
+        else :
+            break
+    
+    cap.release()
+    cv2.destroyAllWindows()
+
+
