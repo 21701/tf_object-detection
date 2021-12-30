cbafd19b39919580bb60fa8c1e4026f2da3f622d
21701 <king2doi@gmail.com>
2021-12-30 11:31:44 +0900

    4444444444

--- object_detection_video.py
+++ object_detection_video.py
@@ -27,6 +27,12 @@ category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABE
 
 # /20200711/efficientdet_d0_coco17_tpu-32.tar.gz
 
+# /20200711/ssd_mobilenet_v2_320x320_coco17_tpu-8.tar.gz
+
+# /20210210/centernet_mobilenetv2fpn_512x512_coco17_od.tar.gz
+
+# /20200711/efficientdet_d0_coco17_tpu-32.tar.gz
+
 # Download and extract model
 def download_model(model_name, model_date):
     base_url = 'http://download.tensorflow.org/models/object_detection/tf2/'
@@ -37,7 +43,7 @@ def download_model(model_name, model_date):
     return str(model_dir)
 
 MODEL_DATE = '20200711'
-MODEL_NAME = 'centernet_hg104_1024x1024_coco17_tpu-32'
+MODEL_NAME = 'ssd_mobilenet_v2_320x320_coco17_tpu-8'
 PATH_TO_MODEL_DIR = download_model(MODEL_NAME, MODEL_DATE)
 
 def load_model(model_dir) :
@@ -87,17 +93,58 @@ def show_inference(detection_model, image_np) :
     cv2.imshow( 'result' , image_np_with_detections)
 
 
+def save_inference(detection_model, image_np, video_writer):
 
+    # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
+    input_tensor = tf.convert_to_tensor(image_np)
+    # The model expects a batch of images, so add an axis with `tf.newaxis`.
+    input_tensor = input_tensor[tf.newaxis, ...]
+
+    # input_tensor = np.expand_dims(image_np, 0)
+    detections = detection_model(input_tensor)
+#     print(detections)
+    # All outputs are batches tensors.
+    # Convert to numpy arrays, and take index [0] to remove the batch dimension.
+    # We're only interested in the first num_detections.
+    num_detections = int(detections.pop('num_detections'))
+    detections = {key: value[0, :num_detections].numpy()
+                   for key, value in detections.items()}
+    detections['num_detections'] = num_detections
+
+    # detection_classes should be ints.
+    detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+    
+    print(detections)
+    image_np_with_detections = image_np.copy()
+
+    viz_utils.visualize_boxes_and_labels_on_image_array(
+          image_np_with_detections,
+          detections['detection_boxes'],
+          detections['detection_classes'],
+          detections['detection_scores'],
+          category_index,
+          use_normalized_coordinates=True,
+          max_boxes_to_draw=200,
+          min_score_thresh=.30, # 예측한 네모박스가 적어도 확신이 30% 이상 되는것만 박스를 그린다
+          agnostic_mode=False)
+
+    video_writer.write(image_np_with_detections)
 
 
 
 # 비디오를 실행하는 코드
-cap = cv2.VideoCapture('data/video.mp4')
+cap = cv2.VideoCapture('data/dashcam2.mp4')
+# cap = cv2.VideoCapture(0) # 카메라 캠
 
 if cap.isOpened() == False :
     print('비디오 실행에러')
 
 else :
+    frame_width = int( cap.get(3) )
+    frame_height = int( cap.get(4) )
+    out = cv2.VideoWriter('data/output.avi', cv2.VideoWriter_fourcc('M','J','P','g'), 20, (frame_width, frame_height))
+
+
     # 비디오 캡쳐해서 이미지를 1장씩 가져온다
     # 이 1장의 이미지를 오브젝트 디텍션 한다
     while cap.isOpened() :
@@ -111,9 +158,10 @@ else :
             # 수정하세요 
 
             start_time = time.time()
+            # 동영상을 디텍션 한 후, 파일로 저장하는 함수
+            # save_inference(detection_model, frame, out)
 
-            # save_inference(detection_model, frame)
-
+            # 돋영상을 실시간으로 바로 화면에서 디텍팅하는것
             show_inference(detection_model, frame)
 
             end_time = time.time()
@@ -126,6 +174,7 @@ else :
             break
     
     cap.release()
+    out.release()
     cv2.destroyAllWindows()
 
 
